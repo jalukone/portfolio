cc1e0a1c224d75fc12ba1fd298e13019bd76cbbc
JalukOne <jalukone@outlook.com>
2023-01-02 21:45:33 -0500

    export json

---
+++ src/utils/icons-tech.tsx
@@ -0,0 +1,47 @@
+// L A N G U A J E S
+import Typescript from "../assets/typescript.svg"
+import Javascript from "../assets/javascript.svg"
+import Html from "../assets/html.svg"
+import Css from "../assets/css.svg"
+import Sql from "../assets/sql.svg"
+
+export const languages = [
+    { id: "typescript", path: Typescript },
+    { id: "javascript", path: Javascript },
+    { id: "html", path: Html },
+    { id: "css", path: Css },
+    { id: "sql", path: Sql },
+]
+
+// F R A M E W O R K S
+import React from "../assets/react.svg"
+import Nodejs from "../assets/node-js.svg"
+import Expressjs from "../assets/expressjs.svg"
+import Tailwindcss from "../assets/tailwindcss.svg"
+
+export const frameworks = [
+    { id: "react", path: React },
+    { id: "nodejs", path: Nodejs },
+    { id: "expressjs", path: Expressjs },
+    { id: "tailwindcss", path: Tailwindcss },
+]
+
+// T O O L S
+import Git from "../assets/git.svg"
+import Docker from "../assets/docker.svg"
+import Mongodb from "../assets/mongodb.svg"
+
+export const tools = [
+    { id: "git", path: Git },
+    { id: "docker", path: Docker },
+    { id: "mongodb", path: Mongodb },
+]
+
+// D E S I G N
+import Figma from "../assets/figma.svg"
+
+export const design = [
+    { id: "figma", path: Figma }
+]
+
+
